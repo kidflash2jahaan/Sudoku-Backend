247a9f991aca825eadafb5ca720b05f5f7f08193
Jahaan Pardhanani <kidflash2jahaan@gmail.com>
2024-10-21 06:42:43 -0700

    Add CORS

--- src/main/java/com/systemerr/sudokubackend/SudokuBackendApplication.java
+++ src/main/java/com/systemerr/sudokubackend/SudokuBackendApplication.java
@@ -3,12 +3,25 @@ package com.systemerr.sudokubackend;
 import com.mongodb.client.MongoClients;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class SudokuBackendApplication {
     public static void main(String[] args) {
         SpringApplication.run(SudokuBackendApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**");
+            }
+        };
     }
 }
